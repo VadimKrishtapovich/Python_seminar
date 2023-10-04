8a43f0f53635b73c22c6b496726b4a0b894aa0fc
Vadim <vadim.krishtapovich@mail.ru>
2023-10-04 20:28:27 +0300

    Homework_3

--- Homework_3/main.py
+++ Homework_3/main.py
@@ -53,4 +53,17 @@
 #     power_of_two *= 2
 
 
+# Задача
+
+# var1 = '5 4' # количество элементов первого и второго множества
+# var2 = '1 3 5 7 9' # элементы первого множества через пробел
+# var3 = '2 3 4 5' # элементы второго множества через пробел
+
+# n, m = map(int, var1.split())
+# set1 = set(map(int, var2.split()))
+# set2 = set(map(int, var3.split()))
+# common_numbers = sorted(set1.intersection(set2))
+# result = ' '.join(map(str, common_numbers))
+# print (result)
+
 
