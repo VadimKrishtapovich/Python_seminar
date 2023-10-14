fa3271af6fc02960095b9a64e1f4d40e4835b39b
Vadim <vadim.krishtapovich@mail.ru>
2023-10-14 10:13:46 +0300

    Работа с файлами

--- Seminar_8/logger.py
+++ Seminar_8/logger.py
@@ -2,7 +2,7 @@ from data_create import input_user_data
 
 def input_data():
     name, surname, phone, adress = input_user_data()
-    var = int(input(f'\nВ каком формате записатьданные?\n'
+    var = int(input(f'\nВ каком формате записать данные?\n'
                     f'1 Вариант‚:\n'
                     f'{name}\n'
                     f'{surname}\n'
@@ -37,3 +37,42 @@ def print_data():
         for i in data:
             if i != '\n':
                 print(i, end="")
+
+def modify_data():
+    name = input("Введите имя или фамилию, данные которого нужно изменить: ")
+    data = [{"имя": "Иван", "фамилия": "Иванов", "возраст": 25}]
+    for record in data:
+        if record["имя"] == name or record["фамилия"] == name:
+            new_name = input("Введите новое имя: ")
+            new_surname = input("Введите новую фамилию: ")
+            # Найдена соответствующая запись
+            # Запросить новые данные
+            record["имя"] = new_name
+            record["фамилия"] = new_surname
+            print("Данные изменены успешно.")
+        else:
+            print("Запись с указанным именем или фамилией не найдена.")
+
+
+def delete_data():
+    name = input("Введите имя или фамилию, данные которого нужно удалить: ")
+      
+    with open('data_first_variant.csv', 'w', encoding="utf-8") as file:
+        reader = file.readlines()
+        data = list(reader)
+    
+    deleted_count = 0
+    
+    with open('data_first_variant.csv', 'w', newline='') as file:
+        writer = delete_data(file)
+        for row in data:
+            if name.lower() not in [cell.lower() for cell in row]:
+                writer.writerow(row)
+            else:
+                deleted_count += 1
+    
+    if deleted_count > 0:
+        print("Данные удалены успешно.")
+    else:
+        print("Нет данных для удаления по указанному имени или фамилии.")
+    print("Данные удалены успешно.")
--- Seminar_8/ui.py
+++ Seminar_8/ui.py
@@ -1,16 +1,44 @@
-from logger import input_data, print_data
+# from logger import input_data, print_data
+
+# def interface():
+#     print("Добрый день ! Это бот-помощник. \n"
+#           "Что вы хотите сделать?\n"
+#           "1 - Записать данные \n"
+#           "2 - Вывести данные")
+#     command = int(input("Ваш выбор: "))
+
+#     while command < 1 or command > 2:
+#         command = int(input("Ошибка! Ваш выбор: "))
+
+#     if command == 1:
+#         input_data()
+#     elif command == 2:
+#         print_data()
+# ----------------------------------------------------------------
+
+from logger import input_data, print_data, modify_data, delete_data 
 
 def interface():
-    print("Добрый день ! Это бот-помощник. \n"
+    print("Добрый день! Это бот-помощник.\n"
           "Что вы хотите сделать?\n"
-          "1 - Записать данные \n"
-          "2 - Вывести данные")
+          "1 - Записать данные\n"
+          "2 - Вывести данные\n"
+          "3 - Изменить данные\n"
+          "4 - Удалить данные")
     command = int(input("Ваш выбор: "))
 
-    while command < 1 or command > 2:
+    while command < 1 or command > 4:
         command = int(input("Ошибка! Ваш выбор: "))
 
     if command == 1:
         input_data()
     elif command == 2:
         print_data()
+    elif command == 3:
+        modify_data()
+    elif command == 4:
+        delete_data()
+
+
+
+   
